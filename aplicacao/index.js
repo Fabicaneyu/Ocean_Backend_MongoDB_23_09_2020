5bb7e78018013ac3eecaf54e18799b734193586b
Fabi <fabicaneyu@gmail.com>
2020-09-23 16:37:29 -0300

    iniciada conexão projeto vscode com mongodb

--- aplicacao/index.js
+++ aplicacao/index.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongodb = require('mongodb');
 
+// criando um bloco assincrono e já executando esse bloco e tudo ttá dentro 
+(async () => {
+
+const connectionString = 'mongodb+srv://admin:20Mavibifa@cluster0.mpcr5.mongodb.net/admin?retryWrites=true&w=majority';
+
+//async/await ação que começa e espera um tempo para acontecer
+console.info('Conectando ao banco de dados MongoDB!')
+const client = mongodb.MongoClient.connect(connectionString, {
+    useUnifiedTopology: true 
+});
+
+console.info('Banco de dados conectado com sucesso')
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -41,3 +54,6 @@ app.delete('/mensagens/:id', (req, res) => {
 app.listen(port, () => {
     console.log(`App rodando em http://localhost:${port}`);
 });
+
+//fecho o bloco assincrono e executo
+})();
